e3c6811a88367ff3990471e6819d8e1bf0c2b60e
agussoul <aalma@mail.austral.edu.ar>
2024-10-03 15:23:32 -0300

    added snippet table

--- src/main/kotlin/app/model/Snippet.kt
+++ src/main/kotlin/app/model/Snippet.kt
@@ -1,4 +1,21 @@
 package app.model
 
-class Snippet {
-}
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import org.jetbrains.annotations.NotNull
+
+@Entity
+@Table
+data class Snippet(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull
+    val id: Int? = null,
+    @NotNull
+    val description: String,
+    @NotNull
+    val code: String
+)
