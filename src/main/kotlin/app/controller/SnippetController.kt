0180a73b445a3d5b8362b8bc356dde4b261f8243
agussoul <aalma@mail.austral.edu.ar>
2024-10-14 12:41:14 -0300

    created endpoints

--- src/main/kotlin/app/controller/SnippetController.kt
+++ src/main/kotlin/app/controller/SnippetController.kt
@@ -2,6 +2,7 @@ package app.controller
 
 import app.model.Snippet
 import app.service.SnippetService
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -22,5 +23,15 @@ class SnippetController(private val snippetService: SnippetService) {
     @PostMapping("/add/snippet")
     fun addSnippet(@RequestParam snippet: Snippet): Mono<Snippet> {
         return snippetService.addSnippet(snippet)
+    }
+
+    @DeleteMapping("/delete/snippet")
+    fun deleteSnippet(@RequestParam snippetId: Int): Mono<String> {
+        return snippetService.deleteSnippet(snippetId)
+    }
+
+    @GetMapping("/get/snippet")
+    fun getSnippet(@RequestParam snippetId: Int): Mono<Snippet> {
+        return snippetService.getSnippet(snippetId)
     }
 }
