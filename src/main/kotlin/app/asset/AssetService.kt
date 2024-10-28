3c7ec25e626f738a14e2edd077b90e0757603bcc
agussoul <aalma@mail.austral.edu.ar>
2024-10-28 20:15:33 -0300

    added asset

---
+++ src/main/kotlin/app/asset/AssetService.kt
@@ -0,0 +1,36 @@
+package com.example.springboot.app.asset
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Component
+import org.springframework.web.client.RestTemplate
+
+@Component
+class AssetService
+    @Autowired
+    constructor(
+        private val restTemplate: RestTemplate,
+        @Value("\${bucket_url")
+        private val bucketUrl: String
+    ) {
+        fun saveSnippet(snippetId: String, code: String): ResponseEntity<String> {
+            try {
+                val request = HttpEntity(code, HttpHeaders())
+                restTemplate.put("$bucketUrl/$snippetId", request)
+                return ResponseEntity.ok(null)
+            } catch (e: Exception) {
+                return ResponseEntity.badRequest().build()
+            }
+        }
+
+        fun getSnippet(snippetId: String): ResponseEntity<String> {
+            return ResponseEntity.ok(
+                restTemplate.getForEntity(
+                    "$bucketUrl/$snippetId", String::class.java
+                ).body!!
+            )
+        }
+    }
