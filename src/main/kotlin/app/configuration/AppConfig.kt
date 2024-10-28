fe783aabd3452ba58b97b7a6985c9a30384e993d
nicolasberretta <neberretta@mail.austral.edu.ar>
2024-10-27 23:03:40 -0300

    added comment in app con

--- src/main/kotlin/app/configuration/AppConfig.kt
+++ src/main/kotlin/app/configuration/AppConfig.kt
@@ -6,24 +6,22 @@ import org.springframework.http.client.ClientHttpResponse
 import org.springframework.web.client.ResponseErrorHandler
 import org.springframework.web.client.RestTemplate
 
+
 @Configuration
 class RestTemplateConfig {
 
-    @Configuration
-    class RestTemplateConfig {
-
-        @Bean
-        fun restTemplate(): RestTemplate {
-            val restTemplate = RestTemplate()
-            restTemplate.errorHandler = object : ResponseErrorHandler {
-                override fun hasError(response: ClientHttpResponse): Boolean {
-                    return response.statusCode.is4xxClientError || response.statusCode.is5xxServerError
-                }
-
-                override fun handleError(response: ClientHttpResponse) {
-                }
+    @Bean
+    fun restTemplate(): RestTemplate {
+        val restTemplate = RestTemplate()
+        restTemplate.errorHandler = object : ResponseErrorHandler {
+            // this avoids the default behavior of RestTemplate to throw exceptions for HTTP error status codes.
+            override fun hasError(response: ClientHttpResponse): Boolean {
+                return response.statusCode.is4xxClientError || response.statusCode.is5xxServerError
             }
-            return restTemplate
+
+            override fun handleError(response: ClientHttpResponse) {
+            }
         }
+        return restTemplate
     }
 }
