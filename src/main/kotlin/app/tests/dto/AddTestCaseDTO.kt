74b4975c208bc68a701c0488ae1a37b74eb2c9f0
agussoul <aalma@mail.austral.edu.ar>
2024-12-01 11:04:35 -0300

    fix update-test

--- src/main/kotlin/app/tests/dto/AddTestCaseDTO.kt
+++ src/main/kotlin/app/tests/dto/AddTestCaseDTO.kt
@@ -3,6 +3,7 @@ package com.example.springboot.app.tests.dto
 import org.jetbrains.annotations.NotNull
 
 data class AddTestCaseDTO(
+    val id: String?,
     @NotNull
     val name: String,
     val input: List<String>?,
