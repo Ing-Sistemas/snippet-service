1224a7e995b39c84fcc527c33a881fc70c1ad184
nicolasberretta <neberretta@mail.austral.edu.ar>
2024-12-06 21:17:45 -0300

    added new methods in rule service

--- src/main/kotlin/app/rules/RulesService.kt
+++ src/main/kotlin/app/rules/RulesService.kt
@@ -2,13 +2,17 @@ package com.example.springboot.app.rules
 
 import com.example.springboot.app.rules.dto.AddRuleDTO
 import com.example.springboot.app.rules.dto.RuleDTO
+import com.example.springboot.app.rules.entity.Rule
 import com.example.springboot.app.rules.entity.RulesUserEntity
 import com.example.springboot.app.rules.enums.RulesetType
 import com.example.springboot.app.rules.repository.RuleRepository
 import com.example.springboot.app.rules.repository.RuleUserRepository
+import com.example.springboot.app.utils.FormatConfig
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import java.util.*
+import kotlin.reflect.full.memberProperties
 
 @Service
 class RulesService
@@ -21,7 +25,31 @@ class RulesService
 
     fun getRules(ruleType: RulesetType, userId: String): List<RuleDTO> {
         logger.info("Getting rules for $ruleType")
-        return ruleRepository.findAllRulesByUserIdAndType(userId, ruleType)
+        val rules = ruleRepository.findAllRulesByUserIdAndType(userId, ruleType)
+
+        if (rules.isEmpty()) {
+            val defaultRules = genDefaultRules(ruleType)
+            defaultRules.forEach {
+                ruleRepository.save(
+                    Rule(
+                        id = it.id,
+                        name = it.name,
+                        value = it.value.toString(),
+                        type = it.ruleType
+                    )
+                )
+                ruleUserRepository.save(
+                    RulesUserEntity(
+                        userId = userId,
+                        isActive = it.isActive,
+                        rule = ruleRepository.findRuleById(it.id),
+                    )
+                )
+            }
+            return defaultRules
+        } else {
+            return rules
+        }
     }
 
     fun updateRules(
@@ -40,7 +68,7 @@ class RulesService
                     RulesUserEntity(
                         userId = userId,
                         isActive = rule.isActive,
-                        rule = ruleRepository.findById(rule.ruleId).get(),
+                        rule = ruleRepository.findRuleById(rule.ruleId),
                     ),
                 )
             }
@@ -48,4 +76,48 @@ class RulesService
         // TODO missing sending to the ps service part
     }
 
+    private fun genDefaultRules(ruleType: RulesetType): List<RuleDTO> {
+        return when (ruleType) {
+            RulesetType.FORMAT -> {
+                val config = FormatConfig(
+                    spaceBeforeColon = false,
+                    spaceAfterColon = false,
+                    spaceAroundEquals = false,
+                    lineJumpBeforePrintln = 0,
+                    lineJumpAfterSemicolon = true,
+                    singleSpaceBetweenTokens = true,
+                    spaceAroundOperators = true
+                )
+                FormatConfig::class.memberProperties.map { property ->
+                    val name = property.name
+                    val value = property.get(config)?.toString() ?: "null"
+
+                    RuleDTO(
+                        id = UUID.randomUUID().toString(),
+                        name = name,
+                        isActive = false,
+                        value = value,
+                        ruleType = RulesetType.FORMAT,
+                    )
+                }
+            }
+
+            RulesetType.LINT -> {
+                val availableRules = listOf(
+                    "identifier_format",
+                    "mandatory-variable-or-literal-in-println",
+                    "mandatory-variable-or-literal-in-readInput",
+                )
+                availableRules.map { ruleName ->
+                    RuleDTO(
+                        id = UUID.randomUUID().toString(),
+                        name = ruleName,
+                        isActive = false,
+                        value = null, // "camel case" or "snake case"
+                        ruleType = RulesetType.LINT,
+                    )
+                }
+            }
+        }
+    }
 }
--- src/main/kotlin/app/rules/repository/RuleRepository.kt
+++ src/main/kotlin/app/rules/repository/RuleRepository.kt
@@ -5,6 +5,7 @@ import com.example.springboot.app.rules.dto.RuleDTO
 import com.example.springboot.app.rules.entity.Rule
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
