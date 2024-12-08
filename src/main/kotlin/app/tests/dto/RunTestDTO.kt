c1709677f2be7039669a1f102a0e09c3aa55e347
Felipe Germano <fgermano@mail.austral.edu.ar>
2024-12-07 22:29:21 -0300

    changezzzzz

--- src/main/kotlin/app/tests/dto/RunTestDTO.kt
+++ src/main/kotlin/app/tests/dto/RunTestDTO.kt
@@ -4,7 +4,7 @@ import com.example.springboot.app.tests.enums.TestStatus
 
 data class RunTestDTO(
     val id: String?,
-    val name: String,
+    val name: String?,
     val input: List<String>?,
     val output: List<String>?,
     val status: TestStatus? = null
--- src/main/kotlin/app/utils/ValidateTestRunRequest.kt
+++ src/main/kotlin/app/utils/ValidateTestRunRequest.kt
@@ -3,6 +3,6 @@ package com.example.springboot.app.utils
 import com.example.springboot.app.tests.dto.RunTestDTO
 
 data class ValidateTestRunRequest(
-    val testCaseDTO: RunTestDTO,
+    val testCaseDTO: RunTestDTO?,
     val sId: String
 )
