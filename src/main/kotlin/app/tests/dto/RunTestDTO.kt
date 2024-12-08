0aeb1e736e7f27605005fade1c0215e41d2ee4e3
Felipe Germano <fgermano@mail.austral.edu.ar>
2024-12-07 21:57:13 -0300

    changezzzz

--- src/main/kotlin/app/tests/dto/RunTestDTO.kt
+++ src/main/kotlin/app/tests/dto/RunTestDTO.kt
@@ -1,8 +1,11 @@
 package com.example.springboot.app.tests.dto
 
+import com.example.springboot.app.tests.enums.TestStatus
+
 data class RunTestDTO(
     val id: String?,
     val name: String,
     val input: List<String>?,
     val output: List<String>?,
+    val status: TestStatus? = null
 )
